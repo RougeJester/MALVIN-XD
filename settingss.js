d2086c9c60e329cd21d0c903ae525165ef01f958
RougeJester <letlotlomosatupa01@gmail.com>
2025-07-23 21:39:44 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: " malvin~YydQjarT#LZwwbjANoN-wJcI713zWddPMvNt_NoKoqoUmieXmCfI",  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
-  PREFIX: "." // prefix (e.g., ., /, !, *)
+  PREFIX: "!" // prefix (e.g., ., /, !, *)
 };
